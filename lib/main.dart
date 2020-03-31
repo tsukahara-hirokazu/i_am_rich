ea979096de47c0b4ffc2a23e30249eb26bba1cfc
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-03-31 12:36:26 +0900

    28_Working with Assets in Flutter & the Pubspec file

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,7 @@ void main() => runApp(
             backgroundColor: Colors.blueGrey[900],
           ),
           backgroundColor: Colors.blueGrey,
-          body: Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://cdn.omni-links.com/tourimages/thumb/f5cb7faf1d7e12d2379695b87aaa64d5_thumb_800x800.jpg'),
-            ),
-          ),
+          body: Center(child: Image(image: AssetImage('images/diamond.png'))),
         ),
       ),
     );
